2b35aff93f360de1420314be7f9620b73c237beb
Erick Lavoie <erick.lavoie@gmail.com>
2011-11-16 14:26:31 -0500

    Removed load statements in ometa-rhino.js

--- ometa-rhino.js
+++ ometa-rhino.js
@@ -1,11 +1,3 @@
-load("lib.js")
-load("ometa-base.js")
-load("parser.js")
-load("bs-js-compiler.js")
-load("bs-ometa-compiler.js")
-load("bs-ometa-optimizer.js")
-load("bs-ometa-js-compiler.js")
-
 alert = print
 
 translateCode = function(s) {
